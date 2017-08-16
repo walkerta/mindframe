b5f70dfc36c527e2cf9599acf94918fcb0575aa2
Tabetha Walker-Smith <walkertasmith@gmail.com>
2017-08-16 13:41:20 -0700

    Fixed layout.

---
+++ frontend/src/components/CustomizePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default class CustomizePage extends React.Component{
+  render () {
+    return (
+
+      <div>CustomizePage
+      </div>
+
+    )
+  }
+
+
+}
