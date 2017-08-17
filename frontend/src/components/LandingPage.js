7d5ed5ef03573e33004d743f82c39bdb5b5ced25
Tabetha Walker-Smith <walkertasmith@gmail.com>
2017-08-17 11:23:36 -0700

    A few more updates.

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,15 +1,46 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Appbar from 'muicss/lib/react/appbar';
+//import {Link} from 'react-router-dom';
+import Panel from 'muicss/lib/react/panel';
+import Container from 'muicss/lib/react/container';
 
+export default class LandingPage extends React.Component {
+  render() {
+    let s1 = {
+      verticalAlign: 'middle'
+    };
+    let s2 = {
+      textAlign: 'right'
+    };
 
-export default class LandingPage extends React.Component{
-  render () {
     return (
 
-      <div>LandingPage
-      </div>
 
-    )
+      <Appbar>
+
+      <table width="100%">
+        <tbody>
+          <tr style={s1}>
+            <td className="mui--appbar-height"></td>
+            <td className="mui--appbar-height" style={s2}>
+
+              <em>Home Theme Views</em>
+            </td>
+          </tr>
+        </tbody>
+        <h1><em>Mindframe</em></h1>
+      </table>
+
+      <Panel>
+        {}
+      </Panel>
+
+
+    </Appbar>
+
+
+
+    );
   }
-
-
 }
