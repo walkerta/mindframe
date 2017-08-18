126fd6933cff615fd6956fae4a7cd56478923bb3
Tabetha Walker-Smith <walkertasmith@gmail.com>
2017-08-18 13:16:48 -0700

    change baselayout

--- frontend/src/components/BaseLayout.js
+++ frontend/src/components/BaseLayout.js
@@ -1,46 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Appbar from 'muicss/lib/react/appbar';
-//import {Link} from 'react-router-dom';
-import Panel from 'muicss/lib/react/panel';
-import Container from 'muicss/lib/react/container';
+import {Link} from 'react-router-dom';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
+
+
+//import Container from 'muicss/lib/react/container';
 
 export default class BaseLayout extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open: false});
   render() {
-    let s1 = {
-      verticalAlign: 'middle'
-    };
-    let s2 = {
-      textAlign: 'right'
-    };
+      return (
 
-    return (
+          <div>
+            <AppBar
+               title="Mindframe"
+               onLeftIconButtonTouchTap={this.handleToggle}
+               iconClassNameRight="muidocs-icon-navigation-expand-more"/>
+            <Drawer
+               docked={false}
+               width={200}
+               open={this.state.open}
+               onRequestChange={(open) => this.setState({open})}
+             >
+               <MenuItem onClick={this.handleClose}>
+                 <Link to="/">Landing Page</Link>
+               </MenuItem>
+               <MenuItem onClick={this.handleClose}>
+                 <Link to="/Customize">Customize Page</Link>
+               </MenuItem>
+             </Drawer>
+             {this.props.children}
+           </div>
 
-
-      <Appbar>
-
-      <table width="100%">
-        <tbody>
-          <tr style={s1}>
-            <td className="mui--appbar-height"></td>
-            <td className="mui--appbar-height" style={s2}>
-
-              <em>Home Theme Views</em>
-            </td>
-          </tr>
-        </tbody>
-        <h1><em>Mindframe</em></h1>
-      </table>
-
-      <Panel>
-        {}
-      </Panel>
-
-
-    </Appbar>
-
-
-
-    );
+      );
   }
 }
